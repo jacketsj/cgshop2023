8aeda307beeaecabdac98f38e8f5732f6ea6e11b
Jack S-J <jacketsj@gmail.com>
2023-01-23 12:52:46 -0500

    Finish flood raycast implementation

--- src/flood.cpp
+++ src/flood.cpp
@@ -5,6 +5,8 @@
 #include <CGAL/Point_2.h>
 #include <CGAL/Polygon_2.h>
 #include <CGAL/Polygon_with_holes_2.h>
+#include <CGAL/ch_graham_andrew.h>
+#include <CGAL/intersections.h>
 #include <algorithm>
 #include <initializer_list>
 #include <iostream>
@@ -19,6 +21,16 @@ using Ray = CGAL::Ray_2<Kernel>;
 using Polygon = CGAL::Polygon_with_holes_2<Kernel>;
 using SimplePolygon = CGAL::Polygon_2<Kernel>;
 
+SimplePolygon extend_to_point(const vector<Point>& pointset, const Point& p) {
+	vector<Point> points = pointset;
+	points.push_back(p);
+	vector<Point> chull;
+	CGAL::ch_graham_andrew(points.begin(), points.end(),
+												 std::back_inserter(chull));
+	SimplePolygon newPoly(chull.begin(), chull.end());
+	return newPoly;
+}
+
 void Flooder::flood_raycast(const Point& p, const Point& dir) {
 	// TODO
 	// compute arrangement (or maintain)
@@ -27,19 +39,70 @@ void Flooder::flood_raycast(const Point& p, const Point& dir) {
 	// binary search along the intersection points
 	// at each one, check if the expansion to include the point (i.e. convex
 	// hull w/ that point) is inside container
-	vector<Point> relevant_points = container_points;
+	vector<Point> current_points;
+	vector<Line> relevant_lines = container_lines;
+	for (const Point& p1 : cur) {
+		current_points.push_back(p1);
+		for (const Point& p2 : container_points)
+			relevant_lines.emplace_back(p1, p2);
+	}
+	for (size_t i = 0; i < current_points.size(); ++i)
+		for (size_t j = i + 1; j < current_points.size(); ++j)
+			relevant_lines.emplace_back(current_points[i], current_points[j]);
+
+	Ray r(p, dir);
+	vector<pair<Kernel::FT, Point>> possible_points;
+	for (auto& line : relevant_lines) {
+		auto intersection = CGAL::intersection(r, line);
+		if (intersection)
+			if (const Point* p0 = boost::get<Point>(&intersection.get()))
+				possible_points.emplace_back(CGAL::squared_distance(p, *p0), *p0);
+	}
+
+	sort(possible_points.begin(), possible_points.end());
+
+	assert(!possible_points.empty());
+	// TODO filter out points in possible points that are outside container
+	// TODO filter out points in possible points that are strictly inside cur
+	// (optional)
+	// TODO sort points in possible points by their distance from p
+	// TODO binary search on points q in possible points, checking if the convex
+	// hull of current_points+q is contained within container each time (not
+	// strictly), i.e. if the union of both expands the area
+	auto is_outside_comp = [&](int y, const pair<Kernel::FT, Point>& qq) {
+		const Point& q = qq.second;
+		// compute convex hull of q merged with cur
+		SimplePolygon newPoly = extend_to_point(current_points, q);
+		bool inside = false;
+		// CGAL oriented side and complement method
+		vector<Polygon> polygon_c;
+		CGAL::complement(container, std::back_inserter(polygon_c));
+		// assert(polygon_c.size() == 1);
+		inside = true;
+		// auto generalNewPoly = GeneralPolygon(newPoly.begin(), newPoly.end());
+		for (auto& outside_piece : polygon_c) {
+			// does the exterior of one intersect the interior of the other
+			inside = inside && CGAL::oriented_side(outside_piece, newPoly) !=
+														 CGAL::ON_POSITIVE_SIDE;
+		}
+		return !inside;
+	};
+	auto first_incorrect = std::upper_bound(
+			possible_points.begin(), possible_points.end(), 0, is_outside_comp);
+	size_t distance = first_incorrect - possible_points.begin();
+	if (distance > 0) {
+		cur = extend_to_point(current_points, possible_points[distance - 1].second);
+	}
 }
 
 void Flooder::run() {
 	Point interior = cur[0];
 	// TODO better orders
-	for (const auto& dir : container.outer_boundary()) {
+	for (const auto& dir : container.outer_boundary())
 		flood_raycast(interior, dir);
-	}
 	for (const auto& hole : container.holes())
-		for (const auto& dir : hole) {
+		for (const auto& dir : hole)
 			flood_raycast(interior, dir);
-		}
 	auto original_area = area(cur);
 	auto cur_area = original_area;
 	do {
